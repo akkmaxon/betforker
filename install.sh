8e83fd18cea201e76373c267e63b20017bfccf1a
Maxim Akkuzin <akkmaxon2307@gmail.com>
2015-07-18 13:41:37 +0300

    Added install.sh script for installing to home folder in Arch based distros

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo 'alias forker="cd ~/forker/; bin/f"' >> ~/.bashrc
+cp config.yml.template config.yml
+source ~/.bashrc
+
+ruby -v
+
+if [ "$?" != 0 ]; then
+    sudo pacman -S ruby
+    if [ "$?" != 0 ]; then
+	echo "Use Arch Linux, please, or install ruby manually"
+	exit 1
+    else
+	echo "Ok, we installed ruby"
+    fi
+fi
+
+phantomjs -v
+
+if [ "$?" != 0 ]; then
+    sudo pacman -S phantomjs
+    if [ "$?" != 0 ]; then
+	echo "Use Arch Linux, please, or install phantomjs manually"
+	exit 1
+    else
+	echo "Ok, we installed phantomjs"
+    fi
+fi
+
+gem install nokogiri capybara poltergeist mechanize
+
+echo "We are ready!"
+
+forker
+if [ "$?" != 0 ]; then
+    echo "Close terminal and open it again"
+fi
