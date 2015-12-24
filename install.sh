3d89acefa2290e563de5ab2166337c01a63df1a9
Olga Akkuzina <olgakorneva12@gmail.com>
2015-12-24 23:34:17 +0000

    Update README and install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# if you understand this script, then you should install next things manually:
-# - ruby and phantomjs
-# - bundler for install all necessary gems
-# - after all add gems path to $PATH for using bundle and install gems
-# - copy config.yml.example to config.yml and change config.yml as you want
-# - bin/f - is executable file
+# - Installing this terrible app on your archlinux based computer.
 
 ruby -v
 
@@ -30,16 +25,28 @@ if [ "$?" != 0 ]; then
     fi
 fi
 
-gem install bundler
+bundle --version
+
+if [ "$?" != 0 ]; then
+    gem install bundler
+    if [ "$?" != 0 ]; then
+	echo "You should install bundle gem manually"
+	exit 1
+    else
+	echo "Ok, we installed bundler"
+    fi
+fi
+
+export PATH=$PATH:$HOME/.gem/ruby/2.2.0/bin
+bundle install
+if [ "$?" != 0 ]; then
+  echo "Some gems are not installed. Try to understand what happened and retype ./install.sh"
+fi
 
 cp config.yml.template config.yml
-
 echo 'export PATH=$PATH:$HOME/.gem/ruby/2.2.0/bin' >> ~/.bashrc
 echo 'alias forker="cd ~/forker/; bin/f"' >> ~/.bashrc
 echo 'alias forker_log="cd ~/forker/; tail -f forker_log"' >> ~/.bashrc
 
+echo "Installation completed. Change forker/config.yml and type 'forker' for beginning"
 exec bash
-bundle install
-
-echo "If you see that gems are not installed, you should install them(nokogiri, capybara, poltergeist, mechanize) manually, and after run 'bundle install'. NOT RUN THIS SCRIPT ANYMORE."
-echo "Change forker/config.yml and type 'forker' for beginning"
