ee5562aaa8d22c1255b945602ef773c8f1b268fb
Maxim Akkuzin <akkmaxon2307@gmail.com>
2015-12-25 00:42:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# if you understand this script, then you should install next things manually:
+# - ruby and phantomjs
+# - bundler for install all necessary gems
+# - after all add gems path to $PATH for using bundle and install gems
+# - copy config.yml.example to config.yml and change config.yml as you want
+# - bin/f - is executable file
 
 ruby -v
 
@@ -24,12 +30,16 @@ if [ "$?" != 0 ]; then
     fi
 fi
 
-gem install nokogiri capybara poltergeist mechanize
+gem install bundler
 
 cp config.yml.template config.yml
 
+echo 'export PATH=$PATH:$HOME/.gem/ruby/2.2.0/bin' >> ~/.bashrc
 echo 'alias forker="cd ~/forker/; bin/f"' >> ~/.bashrc
 echo 'alias forker_log="cd ~/forker/; tail -f forker_log"' >> ~/.bashrc
 
-echo "If you see that gems are not installed, you should install them(nokogiri, capybara, poltergeist, mechanize) manually. NOT RUN THIS SCRIPT ANYMORE."
+exec bash
+bundle install
+
+echo "If you see that gems are not installed, you should install them(nokogiri, capybara, poltergeist, mechanize) manually, and after run 'bundle install'. NOT RUN THIS SCRIPT ANYMORE."
 echo "Change forker/config.yml and type 'forker' for beginning"
